f4d6f57f1d1a654fe2756494c5f2c5a7d739c18f
m.drobot <m.drobot@devlegal.ru>
2019-08-16 09:27:38 +0300

    added block to navblock

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     loop: true,
     nav: true,
+    autoplay:true,
+    autoplayTimeout: 7000,
     responsive: {
       0: {
         items: 1
